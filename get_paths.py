efd5502391d210f7e8289996c572a159f4aa4e1f
AmritTiVo <amrit.srivastava@tivo.com>
2019-12-24 15:26:07 +0530

    Adding to master

--- get_paths.py
+++ get_paths.py
@@ -1,6 +1,6 @@
 import requests
 
-url = input('Give JSON url:')
+url = input('Give JSON WELL url:')
 res = requests.get(url)
 if res.status_code == 200:
     [print(i) for i in list(dict(res.json().get('paths')).keys())]
